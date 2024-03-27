9e10d0a120619eea4e68914b9f7dd3ba0ec4ac45
UberEats54 <144610090+UberEats54@users.noreply.github.com>
2024-03-27 11:41:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const tagHorario = document.querySelector("#tagHorario");
 const date = new Date();
 
 const dias = ["Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado", "Domingo"];
-const materias = ["Cálculo I", "Fundamentos Matemáticos", "ATP", "G.A / ICC", "Lógica Matématica", "---", "---"];
-const professores = ["Renato sainha", "Nelson", "Orlando", "Sara / Professor indefinido", "Renato sainha", "---", "---"];
-const locais = ["Anfiteatro I", "Lab Didático II", "Lab Didático I", "Anfiteatro I / Bloco G", "Anfiteatro I", "---", "---"]
+const materias = ["Cálculo I", "Fundamentos Mat", "ATP", "G.A / ICC", "Lógica Mat", "---", "---"];
+const professores = ["Renato sainha", "Nelson", "Orlando", "Sara / indefinido", "Renato sainha", "---", "---"];
+const locais = ["Anfiteatro I", "Lab Didático II", "Lab Didático I", "Anfi I / Bloco G", "Anfiteatro I", "---", "---"]
 const horarios = ["14h", "8h", "14h", "14h / 19h", "14h", "---", "---"];
 
 let d = date.getDay() - 1;
